1a50faad586dc109c9f1a64d411d2c0cb203cd6b
ftmvclr <fatimaavcilar@marun.edu.tr>
2025-12-25 22:53:30 +0300

    Update heap.c

--- heap.c
+++ heap.c
@@ -5,6 +5,13 @@
 #include <math.h>
 #define _CRT_SECURE_NO_WARNINGS 
 
+/*rn i store all the input as a pointer array to process struct
+then i will increment time one by one and check 
+if there are any new processes that should arrive at that time,
+then add that to the queue and find the min again, 
+make it the microP variable, 
+do that process until q time passes */
+
 typedef struct process{
 	struct process *parent;
 	struct process *child;
@@ -33,18 +40,21 @@ int quantum; // optimize for average waiting time
 int process_amount; // how many were there?
 
 double average_wait_time();
-Process *manage_line(char *line);
+void manage_line(char *line, int i);
 void manage_input(FILE *f);
-Process *node_Create(char *id, int t_arr, int e_i);
+BinomialRoot *node_create_returns_root(char *id, int t_arr, int e_i, int i);
+void traverse_nodes_in_q(Process *ptr);
+Process *heapUnion(BinomialRoot *uni);
 void update_processes_in_q(Process *);
+void heapRemove(BinomialRoot *heap, Process *node, Process *before);
+BinomialRoot *heapCreate(Process *node);
 int there_exists_process();
 int anything_new();
 Process *findmin();
-Process *binomial_tree_maker();
 
 int main(){
-	FILE *input = fopen("input.txt", "r");
-	manage_input(input);
+	FILE *file = fopen("input.txt", "r");
+	manage_input(file);
 
 	int i;
 	return 0;
@@ -52,12 +62,10 @@ int main(){
 		quantum = i;
 		while(0){
 			// now send the array to a function that will form all those binomial trees or something
-			binomial_tree_maker(); // let's assume this does reorder everything lmao
 			time++; // time flies
-			if(anything_new()){ // time has passed so we need to know if new stuff arrived
-				binomial_tree_maker();
-			}
-			microP = findmin;
+			anything_new(); // time has passed so we need to know if new stuff arrived
+			
+			microP = findmin();
 			// now that we found the highest priority node, we need to like execute it.
 			microP->t_remains = microP->t_remains - quantum;
 			update_processes_in_q(microP);
@@ -88,17 +96,12 @@ int anything_new(){
 	int i;
 	for(i = 0; i < 10; i++){
 		if(input[i]->t_arr == time)
+			heapUnion();
 			return 1;
 	}
 	return 0;
 }
-Process *binomial_tree_maker(){
-	int i;
-	for(i = 0; i < 3; i++){
-		input[0]; //dumm
-	}
-	return NULL;
-}
+
 // i think this is where we do the "preempting" thingy
 int check_microP(){
 	return 1;
@@ -122,14 +125,14 @@ void update_processes_in_q(Process *p){
 	traverse_nodes_in_q(header);
 }
 
-Process *heapUnion(BinomialRoot *original, BinomialRoot *uni) {
+Process *heapUnion(BinomialRoot *uni) {
 	Process *new_head;
 	Process *prev;
 	Process *aux;
 	Process *next;
 
-	new_head = heapMerge(original, uni);
-	original->processPtr = NULL;
+	new_head = heapMerge(RoR, uni);
+	RoR->processPtr = NULL;
 	uni->processPtr = NULL;
 
 	if(new_head == NULL)
@@ -173,6 +176,128 @@ Process *heapUnion(BinomialRoot *original, BinomialRoot *uni) {
 	return new_head;
 }
 
+void heapRemove(BinomialRoot *heap, Process *node, Process *before){
+	BinomialRoot *temp;
+	Process *child;
+	Process *new_head;
+	Process *next;
+
+	if(node == heap->processPtr)
+		heap->processPtr = node->sibling;
+	else if(before != NULL)
+		before->sibling = node->sibling;
+
+	new_head = NULL;
+	child = node->child;
+
+	while(child != NULL) {
+		next = child->sibling;
+		child->sibling = new_head;
+		child->parent = NULL;
+		new_head = child;
+		child = next;
+	}
+
+	temp = heapInit();
+	if(temp == NULL)
+		return;
+
+	temp->processPtr = new_head;
+	heap->processPtr = heapUnion(heap, temp);
+	free(temp);
+}
+
+BinomialRoot *heapInit(void){
+	BinomialRoot *heap;
+
+	heap = (BinomialRoot *)malloc(sizeof(BinomialRoot));
+	if(heap == NULL)
+		return NULL;
+
+	heap->processPtr = NULL;
+	return heap;
+}
+
+void manage_input(FILE *f){
+	int i = 0;
+	char line[12];
+	while(fgets(line, 12, f) != NULL && i < 10){
+		line[11] = '\0';
+		manage_line(line, i);
+		i++;
+	}
+	for(i = 0; i < 10 && input[i] != NULL; i++){ 
+		printf("created node --- id: %s, ei: %d, t_arr: %d\n",
+			input[i]->process_id, input[i]->e_i, input[i]->t_arr);
+	}
+	process_amount = i; // does work don't change
+}
+ 
+void manage_line(char *line, int i){
+	char *id = strtok(line, " \t\n");
+	char *eiptr = strtok(NULL, " \t\n");
+	char *t_arrptr = strtok(NULL, " \t\n");
+
+	int ei = atoi(eiptr);
+	int t_arr = atoi(t_arrptr);
+	
+	node_create_returns_root(id, t_arr, ei, i);
+}
+
+BinomialRoot *node_create_returns_root(char *id, int t_arr, int e_i, int i){
+
+	Process *node = (Process *)malloc(sizeof(Process));
+	if(node == NULL) return NULL;
+
+	node->parent = NULL;
+	node->child = NULL;
+	node->sibling = NULL;
+	node->degree = 0;
+	strcpy_s(node->process_id, 4, id);
+	node->e_i = e_i;
+	node->t_remains = e_i;
+	node->t_arr = t_arr;
+	node->nth_rodeo = 1;
+	node->total_waited_time = 0;
+	node->pri_factor = e_i;
+
+	input[i] = node;
+	return heapCreate(node);
+}
+
+BinomialRoot *heapCreate(Process *node){
+	BinomialRoot *heap;
+
+	heap = (BinomialRoot *)malloc(sizeof(BinomialRoot));
+	if(heap == NULL)
+		return NULL;
+
+	heap->processPtr = node;
+	heap->nextRoot = NULL; // addition
+	return heap;
+}
+
+double average_wait_time(){ // all nodes
+	int i; double t = 0;
+	for(i = 0; i < process_amount; i++){
+		t += input[i]->total_waited_time;
+	}
+	return t / process_amount;
+}
+
+int there_exists_process(){
+	if(RoR == NULL)
+		return 0;
+	else
+		return 1;
+}
+
+/*void heapFree(BinomialRoot *heap){
+	while(heapMin(heap) != NULL);
+	free(heap);
+}*/
+
+// don't call this function in main. union will call it
 Process *heapMerge(BinomialRoot *heap1, BinomialRoot *heap2) {
 	Process *head;
 	Process *tail;
@@ -212,132 +337,4 @@ Process *heapMerge(BinomialRoot *heap1, BinomialRoot *heap2) {
 
 	tail->sibling = (h1It != NULL) ? h1It : h2It;
 	return head;
-}
-
-Process *node_Create(char *id, int t_arr, int e_i){
-
-	Process *node = (Process *)malloc(sizeof(Process));
-	if(node == NULL) return NULL;
-
-	node->parent = NULL;
-	node->child = NULL;
-	node->sibling = NULL;
-	node->degree = 0;
-	strcpy_s(node->process_id, 4, id);
-	node->e_i =e_i;
-	node->t_remains = e_i;
-	node->t_arr = t_arr;
-	node->nth_rodeo = 1;
-	node->total_waited_time = 0;
-	node->pri_factor = e_i;
-
-	return node;
-}
-
-BinomialRoot *heapCreate(){
-	BinomialRoot *heap;
-
-	heap = (BinomialRoot *)malloc(sizeof(BinomialRoot));
-	if(heap == NULL)
-		return NULL;
-
-	heap->processPtr = NULL;
-	heap->nextRoot = NULL; // addition
-	return heap;
-}
-
-void heapInsert(BinomialRoot *heap, int value)
-{
-	BinomialRoot *temp;
-	Process *node;
-
-	node = nodeInit(value);
-	if(node == NULL)
-		return;
-
-	temp = heapInit();
-	if(temp == NULL)
-		return;
-
-	temp->processPtr = node;
-	heap->processPtr = heapUnion(heap, temp);
-	free(temp);
-}
-
-void heapRemove(BinomialRoot *heap, Process *node, Process *before)
-{
-	BinomialRoot *temp;
-	Process *child;
-	Process *new_head;
-	Process *next;
-
-	if(node == heap->processPtr)
-		heap->processPtr = node->sibling;
-	else if(before != NULL)
-		before->sibling = node->sibling;
-
-	new_head = NULL;
-	child = node->child;
-
-	while(child != NULL) {
-		next = child->sibling;
-		child->sibling = new_head;
-		child->parent = NULL;
-		new_head = child;
-		child = next;
-	}
-
-	temp = heapInit();
-	if(temp == NULL)
-		return;
-
-	temp->processPtr = new_head;
-	heap->processPtr = heapUnion(heap, temp);
-	free(temp);
-}
-
-void manage_input(FILE *f){
-	int i = 0;
-	char line[12];
-	while(fgets(line, 12, f) != NULL && i < 10){
-		line[11] = '\0';
-		input[i++] = manage_line(line);
-	}
-	for(i = 0; i < 10 && input[i] != NULL; i++){ 
-		printf("created node --- id: %s, ei: %d, t_arr: %d\n",
-			input[i]->process_id, input[i]->e_i, input[i]->t_arr);
-	}
-	process_amount = i; // does work don't change
-}
- 
-Process *manage_line(char *line){
-	char *id = strtok(line, " \t\n");
-	char *eiptr = strtok(NULL, " \t\n");
-	char *t_arrptr = strtok(NULL, " \t\n");
-
-	int ei = atoi(eiptr);
-	int t_arr = atoi(t_arrptr);
-	
-	Process *createdNode = node_Create(id, t_arr, ei);
-	return createdNode;
-}
-
-double average_wait_time(){ // all nodes
-	int i; double t = 0;
-	for(i = 0; i < process_amount; i++){
-		t += input[i]->total_waited_time;
-	}
-	return t / process_amount;
-}
-
-int there_exists_process(){
-	if(RoR == NULL)
-		return 0;
-	else
-		return 1;
-}
-
-void heapFree(BinomialRoot *heap) {
-	while(heapMin(heap) != NULL);
-	free(heap);
 }
